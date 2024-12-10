e50249d39b7d34f11161bcdb47f96a9f2b191201
Chandan Sayed <hangzhou635@gmail.com>
2024-12-11 04:23:55 +0600

    MongoDB connected through mongoose

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+// import packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
+// import files
+const connectDB = require("./config/db");
 
 const app = express();
 
@@ -12,5 +16,7 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Server is running!!");
 });
+
+connectDB();
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
